4a6a2250b1b66a32bc6aae151d063a0eb81ca1ef
Javanseal <agomjavan@gmail.com>
2024-04-03 16:27:49 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
 
-***Define class square.***
 
 class Square:
-    ***Defines a square.***
+    """Class that defines properties of square.
+    """
     pass
