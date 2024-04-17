6eefec3c4fe289bfcd10a081e2776422d73e8094
Javanseal <agomjavan@gmail.com>
2024-04-17 06:56:32 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def read_file(filename=""):
+def read_file(filename="my read file"):
     with open(filename, "r", encoding="UTF-8") as file:
         for line in file:
         print(f.read(), end="")
